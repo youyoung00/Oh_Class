2a2021fc3ac79804a76ed3ab241f8e494fc05c6a
youyoung00 <you889@hanmail.net>
2021-11-04 14:40:40 +0900

    "보기 쉽게 주석 제거 및 코드정리"

--- lib/lecture/cls1103.dart
+++ lib/lecture/cls1103.dart
@@ -1,27 +1,10 @@
 import 'dart:math';
 
 void main() {
-
   Cleric cleric = Cleric(name: "유신환", hp: 50, mp: 1);
 
-//   print('''
-// 성직자 캐릭터의 이름은 ${cleric.name}입니다.
-// 성직자의 HP는 초기치 ${cleric.hp}, 최대 ${cleric.maxHp} 입니다.
-// 성직자의 MP는 초기치 ${cleric.mp}, 최대 ${cleric.maxMp} 입니다.''');
-
-  // print("성직자의 hp가 ${cleric.hp - 10}이 되었습니다.");
-  // cleric.selfAid();
-  // print("selfAid를 사용하였습니다. mp는 ${cleric.mp}입니다");
-  // print("selfAid를 사용하였습니다. hp는 ${cleric.hp}입니다");
-
-  // print(cleric.pray(9));
-  // for(int i = 0; i < 10; ){
-  //   print(cleric.pray(6));
-    // print(cleric.pray(i));
-  // }
-
   print("현재 mp는 ${cleric.mp}입니다");
-  print(cleric.pray(6));
+  print("pray 사용!! 현재 mp는 ${cleric.pray(3)}입니다");
 }
 
 class Cleric {
@@ -29,7 +12,7 @@ class Cleric {
   int hp;
   int mp;
 
-  final int maxHp = 50;   // 최대 HP
+  final int maxHp = 50; // 최대 HP
   final int maxMp = 10;
 
   Cleric({this.name, this.hp, this.mp});
@@ -40,15 +23,14 @@ class Cleric {
   }
 
   int pray(int sec) {
-
     num sec1 = sec / 3;
     int sec1Int = sec1.toInt();
 
-    int UpPoint = 3 + Random().nextInt(6-3);
+    int UpPoint = 3 + Random().nextInt(6 - 3);
     int plusMpPoint = UpPoint * sec1Int;
     int finalMp = plusMpPoint + mp;
 
-    if(finalMp > maxMp){
+    if (finalMp > maxMp) {
       finalMp = maxMp;
     } else {
       finalMp;
