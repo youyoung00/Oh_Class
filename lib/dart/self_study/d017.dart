d7f3fa658006741f42ffc5532fc5c5dd36f733f4
youyoung00 <you889@hanmail.net>
2021-11-05 21:43:26 +0900

    "d017 풀이"

--- lib/dart/self_study/d017.dart
+++ lib/dart/self_study/d017.dart
@@ -21,8 +21,20 @@
 // 3
 //
 // 3
+// 1
+
+import 'dart:math';
 
 void main() {
-
+  InputInt inputInt = InputInt();
+  inputInt.inputFunc(9, 12, 3, 6, 10);
 }
 
+class InputInt {
+  void inputFunc(int a, int b, int c, int d, int e) {
+    List<int> inputList = [a, b, c, d, e];
+
+    print(inputList.reduce(max));
+    print(inputList.reduce(min));
+  }
+}
