af00aefcb692be64e481b895ab3b26a4b991484a
youyoung00 <you889@hanmail.net>
2021-11-05 12:38:26 +0900

    반장님 코드

--- lib/lecture/cls1105.dart
+++ lib/lecture/cls1105.dart
@@ -6,6 +6,7 @@ void main() {
   print(word.isVowel(2)); // false
   print(word.isVowel(3)); // false
   print(word.isVowel(4)); // true
+
 }
 
 class Word {
@@ -16,20 +17,27 @@ class Word {
   // i번째 글자가 모음이면 true // a, i, u, e, o
   bool isVowel(int i) {
     bool result = true;
-    final List vowel = ['a', 'i', 'u', 'e', 'o'];
-    if (letters.substring(i, i + 1) == 'a' ||
-        letters.substring(i, i + 1) == 'i') {
-      result;
-    } else if (letters.substring(i, i + 1) == 'u' ||
-        letters.substring(i, i + 1) == 'e') {
-      result;
-    } else if (letters.substring(i, i + 1) == 'o') {
+
+    List<String> vowel = ['a', 'i', 'u', 'e', 'o'];
+
+    if (letters.substring(i, i + 1) == vowel[i]) {
       result;
     } else {
       result = false;
     }
+      // if (letters.substring(i, i + 1) == 'a' ||
+      //     letters.substring(i, i + 1) == 'i') {
+      //   result;
+      // } else if (letters.substring(i, i + 1) == 'u' ||
+      //     letters.substring(i, i + 1) == 'e') {
+      //   result;
+      // } else if (letters.substring(i, i + 1) == 'o') {
+      //   result;
+      // } else {
+      //   result = false;
+      // }
 
-    return result;
+      return result;
   }
 
   // i번째 글자가 자음이면 true
