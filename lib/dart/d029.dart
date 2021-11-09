4c506f9888c48b65994082804011693e5f7dfead
youyoung00 <you889@hanmail.net>
2021-11-09 15:54:17 +0900

    d029 문제제출

--- lib/dart/d029.dart
+++ lib/dart/d029.dart
@@ -6,3 +6,23 @@
 
 // 1
 // 6
+
+void main() {
+  Dice dice = Dice();
+  print(dice.diceNum(3));
+  print(dice.diceNum(1));
+  print(dice.diceNum(10));
+  print(dice.diceNum(0));
+}
+
+class Dice {
+  String diceNum(int n) {
+    int result = 7 - n;
+    String finalResult = result.toString();
+
+    if (n >= 7 || n == 0) {
+      finalResult = '1~6 사이의 숫자를 입력해주세요';
+    }
+    return finalResult;
+  }
+}
