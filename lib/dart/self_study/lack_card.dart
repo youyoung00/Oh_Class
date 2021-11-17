64df5bdc1bd1de94ddcfa16056700de34cca1d54
youyoung00 <you889@hanmail.net>
2021-11-18 02:07:27 +0900

    부족한 카드 문제 코드 개선

--- lib/dart/self_study/lack_card.dart
+++ lib/dart/self_study/lack_card.dart
@@ -45,58 +45,52 @@
 import 'dart:io';
 
 void main() {
-  /// cards 리스트 선언
-  List<int> cards = [];
-
-  /// 첫번째 카드 숫자 입력받기
-  stdout.write('c_');
-  int cardNum1 = int.parse(stdin.readLineSync());
-
-  /// 입력 받은 숫자 cards 리스트에 담기
-  cards.add(cardNum1);
-
-  /// 2번째 카드 숫자 입력받기
-  stdout.write('c_');
-  int cardNum2 = int.parse(stdin.readLineSync());
-  if (cardNum2 == cards[0] || cardNum2 > 5) {
-    print("같은 수, 또는 5보다 큰 수입니다. 프로그램을 재실행 해주세요.");
-  } else {
-    /// 입력 받은 숫자 cards 리스트에 담기
-    cards.add(cardNum2);
-  }
-
-  /// 3번째 카드 숫자 입력받기
-  stdout.write('c_');
-  int cardNum3 = int.parse(stdin.readLineSync());
-  if (cardNum3 == cards[0] || cardNum3 == cards[1]) {
-    print("이전과 같은 수를 입력하였습니다. 프로그램을 재실행 해주세요.");
-  } else if (cardNum3 > 5) {
-    print("5보다 큰 수를 입력하였습니다. 프로그램을 재실행 해주세요.");
-  } else {
-    /// 입력 받은 숫자 cards 리스트에 담기
-    cards.add(cardNum3);
-  }
-
-  /// 4번째 카드 숫자 입력받기
-  stdout.write('c_');
-  int cardNum4 = int.parse(stdin.readLineSync());
-  if (cardNum4 == cards[0] || cardNum4 == cards[1] || cardNum4 == cards[2]) {
-    print("이전과 같은 수를 입력하였습니다. 프로그램을 재실행 해주세요.");
-  } else if (cardNum4 > 5) {
-    print("5보다 큰 수를 입력하였습니다. 프로그램을 재실행 해주세요.");
-  } else {
-    /// 입력 받은 숫자 cards 리스트에 담기
-    cards.add(cardNum4);
-  }
-
-  print(cardReader(cards));
+  CardSearcher cardSearcher = CardSearcher();
+  cardSearcher.inputCards();
 }
 
-int cardReader(List<int> cards) {
-  int result;
+class CardSearcher {
+  List<int> cardList = [];
 
-  int sum = cards[0] + cards[1] + cards[2] + cards[3];
-  result = 15 - sum;
+  void inputCards() {
+    List<int> makeCardList() {
+      stdout.write('c_');
+      int cardNum = int.parse(stdin.readLineSync());
+      if (cardList.contains(cardNum) == true) {
+        print("같은 숫자가 이미 있습니다. 숫자를 다시 입력해주세요.");
+        makeCardList();
+      } else if (cardNum > 5) {
+        print("5보다 큰 숫자를 입력하였습니다. 숫자를 다시 입력해주세요.");
+        makeCardList();
+      } else if (cardNum == 0) {
+        print("0은 없는 카드숫자입니다. 숫자를 다시 입력해주세요.");
+        makeCardList();
+      } else {
+        cardList.add(cardNum);
+      }
 
-  return result;
+      return cardList;
+    }
+
+    makeCardList();
+    makeCardList();
+    makeCardList();
+    makeCardList();
+
+    int cardReader() {
+      int sum = 0;
+
+      for (int i = 0; i < cardList.length; i++) {
+        sum += cardList[i];
+      }
+
+      int result = 15 - sum;
+
+      return result;
+    }
+
+    int finalResult = cardReader();
+
+    print(finalResult);
+  }
 }
