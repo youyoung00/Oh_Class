4710f127812659a49623c77583038f61a2e4fea0
youyoung00 <you889@hanmail.net>
2021-11-24 23:24:01 +0900

    고대문자 문제 제출

--- lib/dart/self_study/typhoon_interval.dart
+++ lib/dart/self_study/typhoon_interval.dart
@@ -59,3 +59,37 @@ void typhoonInterval(List<int> days) {
     print(dayInterval.abs());
   }
 }
+
+// import 'dart:convert';
+// import 'dart:io';
+//
+// void main() {
+//   String a = stdin.readLineSync(encoding: utf8);
+//   String b = stdin.readLineSync(encoding: utf8);
+//   String c = stdin.readLineSync(encoding: utf8);
+//   String d = stdin.readLineSync(encoding: utf8);
+//   String e = stdin.readLineSync(encoding: utf8);
+//
+//   int anumber = int.parse(a);
+//   int bnumber = int.parse(b);
+//   int cnumber = int.parse(c);
+//   int dnumber = int.parse(d);
+//   int enumber = int.parse(e);
+//
+//   List<int> net = [anumber, bnumber, cnumber, dnumber, enumber];
+//   net.sort();
+//
+//
+//   Cal cal = Cal();
+//   print(cal.subtract(net));
+// }
+//
+// class Cal {
+//   int subtract(List<int> net) {
+//     int result = 0;
+//     for (int i = 0; i < 4; i++) {
+//       result = net[i + 1] - net[i];
+//     }
+//     return result;
+//   }
+// }
